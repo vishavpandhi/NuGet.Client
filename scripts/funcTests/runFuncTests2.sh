3b32895854860bafa0f488867267fc5d39b73c20
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2022-06-13 17:36:51 -0700

    some trial and error

--- scripts/funcTests/runFuncTests2.sh
+++ scripts/funcTests/runFuncTests2.sh
@@ -27,8 +27,8 @@ chmod +x cli/dotnet-install.sh
 echo "cli/dotnet-install.sh --install-dir cli --runtime dotnet --channel 3.1 -nopath"
 cli/dotnet-install.sh --install-dir cli --runtime dotnet --channel 3.1 -nopath
 
-echo "cli/dotnet-install.sh --install-dir cli --runtime dotnet --channel 5.0 -nopath"
-cli/dotnet-install.sh --install-dir cli --runtime dotnet --channel 5.0 -nopath
+echo "cli/dotnet-install.sh --install-dir cli --channel $1 --quality GA -nopath"
+cli/dotnet-install.sh --install-dir cli --channel $1 --quality GA -nopath
 
 if (( $? )); then
 	echo "The .NET CLI Install failed!!"
@@ -52,15 +52,6 @@ CMD_OUT_LINES=(`dotnet msbuild build/config.props /v:m /nologo /t:GetCliBranchFo
 CMD_LAST_LINE=${CMD_OUT_LINES[@]:(-1)}
 DOTNET_BRANCHES=${CMD_LAST_LINE//[[:space:]]}
 unset IFS
-
-echo "cli/dotnet-install.sh --install-dir cli --channel $1 --quality GA -nopath"
-cli/dotnet-install.sh --install-dir cli --channel $1 --quality GA -nopath
-
-if (( $? )); then
-	echo "The .NET CLI Install for $DOTNET_BRANCH failed!!"
-	exit 1
-fi
-
 
 # Display .NET CLI info
 $DOTNET --info
@@ -102,8 +93,8 @@ then
 fi
 
 # restore packages
-echo "dotnet msbuild build/build.proj /t:Restore /p:VisualStudioVersion=16.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta /bl:$BUILD_STAGINGDIRECTORY/binlog/02.Restore.binlog"
-dotnet msbuild build/build.proj /t:Restore /p:VisualStudioVersion=16.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta /bl:$BUILD_STAGINGDIRECTORY/binlog/02.Restore.binlog
+echo "dotnet msbuild build/build.proj /t:Restore /p:VisualStudioVersion=17.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta /bl:$BUILD_STAGINGDIRECTORY/binlog/02.Restore.binlog"
+dotnet msbuild build/build.proj /t:Restore /p:VisualStudioVersion=17.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta /bl:$BUILD_STAGINGDIRECTORY/binlog/02.Restore.binlog
 
 if [ $? -ne 0 ]; then
 	echo "Restore failed!!"
@@ -113,8 +104,8 @@ fi
 echo "Restore finished at `date -u +"%Y-%m-%dT%H:%M:%S"`"
 
 # Unit tests
-echo "dotnet msbuild build/build.proj /t:CoreUnitTests /p:VisualStudioVersion=16.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta /bl:$BUILD_STAGINGDIRECTORY/binlog/03.CoreUnitTests.binlog"
-dotnet msbuild build/build.proj /t:CoreUnitTests /p:VisualStudioVersion=16.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta /bl:$BUILD_STAGINGDIRECTORY/binlog/03.CoreUnitTests.binlog
+echo "dotnet msbuild build/build.proj /t:CoreUnitTests /p:VisualStudioVersion=17.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta /bl:$BUILD_STAGINGDIRECTORY/binlog/03.CoreUnitTests.binlog"
+dotnet msbuild build/build.proj /t:CoreUnitTests /p:VisualStudioVersion=17.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta /bl:$BUILD_STAGINGDIRECTORY/binlog/03.CoreUnitTests.binlog
 
 echo "CoreUnitTests failed!! exitcode is $?"
 if [ $? -ne 0 ]; then
@@ -123,9 +114,9 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "================== try to run a single test ==============="
-echo "cli/dotnet test $BUILD_REPOSITORY_LOCALPATH/test/NuGet.Core.Tests/NuGet.Common.Test/bin/Debug/netcoreapp3.1/NuGet.Common.Test.dll --verbosity  detailed --blame-crash-collect-always"
-cli/dotnet test $BUILD_REPOSITORY_LOCALPATH/test/NuGet.Core.Tests/NuGet.Common.Test/bin/release/netcoreapp3.1/NuGet.Common.Test.dll --verbosity  detailed --logger "console;verbosity=detailed" --blame-crash-collect-always
-echo "single test failed!! exitcode is $?"
+echo "cli/dotnet test $BUILD_REPOSITORY_LOCALPATH/test/NuGet.Core.Tests/NuGet.Common.Test/bin/Debug/net5.0/NuGet.Common.Test.dll --verbosity  detailed --blame-crash-collect-always"
+cli/dotnet test $BUILD_REPOSITORY_LOCALPATH/test/NuGet.Core.Tests/NuGet.Common.Test/bin/release/net5.0/NuGet.Common.Test.dll --verbosity  detailed --logger "console;verbosity=detailed" --blame-crash --blame-crash-collect-always --diag:log.txt -f net5.0
+echo "single test exitcode is $?"
 
 echo "Core tests finished at `date -u +"%Y-%m-%dT%H:%M:%S"`"
 
