15624fd7d70f2a84d33041c1e1e289e0afd71e3b
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2022-06-14 20:01:27 -0700

    script update

--- scripts/funcTests/runFuncTests2.sh
+++ scripts/funcTests/runFuncTests2.sh
@@ -123,7 +123,7 @@ fi
 
 echo "================== try to run a single test ==============="
 echo "cli/dotnet test $BUILD_REPOSITORY_LOCALPATH/test/NuGet.Core.Tests/NuGet.Common.Test/bin/Debug/net5.0/NuGet.Common.Test.dll --verbosity  detailed --blame-crash-collect-always"
-cli/dotnet test $BUILD_REPOSITORY_LOCALPATH/test/NuGet.Core.Tests/NuGet.Common.Test/bin/release/netcoreapp3.1/NuGet.Common.Test.dll --verbosity  detailed --logger "console;verbosity=detailed" --blame-crash --blame-crash-collect-always --diag:$(Build.Repository.LocalPath)/log/log1.txt
+cli/dotnet test $BUILD_REPOSITORY_LOCALPATH/test/NuGet.Core.Tests/NuGet.Common.Test/bin/release/netcoreapp3.1/NuGet.Common.Test.dll --verbosity  detailed --logger "console;verbosity=detailed" --blame-crash --blame-crash-collect-always --diag:$BUILD_REPOSITORY_LOCALPATH/log/log1.txt
 echo "single test exitcode is $?"
 
 echo "Core tests finished at `date -u +"%Y-%m-%dT%H:%M:%S"`"
